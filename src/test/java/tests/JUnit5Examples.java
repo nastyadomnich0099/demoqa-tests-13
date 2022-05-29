1a5a8aca0a242e1bfc0b009ffb0e7a82dcb8ebbf
Nastya Domnich <nastyadomnich0711@gmail.com>
2022-05-29 12:10:48 +0400

    added jUnit5

---
+++ src/test/java/tests/JUnit5Examples.java
@@ -0,0 +1,38 @@
+package tests;
+
+
+import org.junit.jupiter.api.*;
+
+public class JUnit5Examples {
+    @BeforeAll
+  static void beforeAll(){
+      System.out.println("==== Smth before all tests");
+    }
+
+    @BeforeEach
+   void beforeEach(){
+        System.out.println("==== Smth before each tests");
+    }
+
+
+    @Test
+    void firstTest(){
+        System.out.println("======= Started First Test");
+    }
+
+    @Test
+    void secondTest(){
+        System.out.println("========= Started second Test");
+    }
+
+    @AfterAll
+    static void afterAll(){
+        System.out.println("==== Smth after all tests");
+    }
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("==== Smth after all tests");
+    }
+
+}
