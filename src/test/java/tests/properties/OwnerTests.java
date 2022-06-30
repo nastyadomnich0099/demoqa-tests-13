adaedf8d3bc510665aa78d90360c0c6c9695d20b
Nastya Domnich <nastyadomnich0711@gmail.com>
2022-06-30 08:21:35 +0300

    added prop_test changes jenkins1

--- src/test/java/tests/properties/OwnerTests.java
+++ src/test/java/tests/properties/OwnerTests.java
@@ -10,17 +10,14 @@ import static java.lang.String.format;
 public class OwnerTests {
     CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
 
-
     @Test
     @Tag("owner")
 
     void someTest1() {
         String login = config.login();
         String password = config.password();
-
         System.out.println("Login: " + login);
         System.out.println("Password: " + password);
-
         String message = format("I logged in as %s with password %s", login, password);
         System.out.println("Message: " + message);
 
